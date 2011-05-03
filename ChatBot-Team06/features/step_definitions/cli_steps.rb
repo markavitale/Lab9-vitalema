f2fb992fad65834b1ec638eece7dc2f90b8c6e5f
Mark Vitale <vitalema@rose-hulman.edu>
2011-05-02 22:35:29 -0400

    adding files for the lab

---
+++ ChatBot-Team06/features/step_definitions/cli_steps.rb
@@ -0,0 +1,10 @@
+Given /^the application is running$/ do
+run_interactive(unescape("ruby chatbot.rb"))
+end
+Then /^I should see "([^""]*)"$/ do |text|
+assert_partial_output(text)
+end
+
+When /^I type "([^"]*)" and press Enter$/ do |input|
+type (input) 
+end
